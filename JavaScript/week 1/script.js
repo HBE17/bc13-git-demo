faed7bfe292bff3034363bafa3b991d2f46d0eb3
HBE17 <hicham-boutaghou@hotmail.co.uk>
2022-09-28 12:16:24 +0100

    added index.html

---
+++ JavaScript/week 1/script.js
@@ -0,0 +1,20 @@
+// let yourAge = prompt('Please eneter your age');
+// let userAge = Number(yourAge);
+
+//         console.log(typeof userAge, typeof yourAge)
+
+//         if (userAge < 18) {
+//             console.log("Get out of here")
+//         } else {
+//           console.log('welcome!')
+//         }
+
+        // let count = 0;
+        // while (count < 10) {
+        //     console.log('hi'+ count);
+        //     count = count + 1;
+        // }
+
+        // for(let count = 0; count < 10; count = count +1){
+        //     console.log('hi' + count)
+        // }
